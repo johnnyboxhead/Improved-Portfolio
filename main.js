3cc8082ba8ba50fde4a4f8ceaefeb2cb02da7fc4
Johnny <johnnyheath@hotmail.com>
2018-05-30 10:51:53 -0400

    Added more parallax, bio div, and additional pictures.

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function parallax(){
     $("#parallax1").css("background-position", "center "+((wScroll*.75)*-1)+"px");
     $("#parallax2").css("background-position", "center "+(180+(((wScroll*.5)*.5)*-1))+"px");
     $("#AboutMeDiv1").css("margin-top", (0+((wScroll*15)/625))+"%");
-    $("#parallax3").css("background-position", "center "+(140+(((wScroll*.2)*.5)*-1))+"px");
-    $("#AboutMeDiv2").css("margin-top", (-10+((wScroll*12)/625))+"%");
+    $("#parallax3").css("background-position", "center "+(120+(((wScroll*.2)*.5)*-1))+"px");
+    $("#AboutMeDiv2").css("margin-top", (-15+((wScroll*12)/625))+"%");
+    $("#ParisSelfie").css("background-position", "right " + (500+(((wScroll*.5)*.5)*-1))+"px");
 }
