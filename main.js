5af84f8ce82eac769c68323342e251a7b4294fa7
Johnny <johnnyheath@hotmail.com>
2018-06-02 19:44:08 -0400

    Continuing to work on mobile responsiveness

--- main.js
+++ main.js
@@ -16,10 +16,14 @@ function parallax(){
     $("#AboutMeDiv1").css("margin-top", (0+((wScroll*12)/625))+"%");
     $("#AboutMeDiv1-2").css("margin-top", (5+((wScroll*10)/625))+"%");
     $("#parallax3").css("background-position", "center "+(120+(((wScroll*.2)*.5)*-1))+"px");
-    $("#AboutMeDiv2").css("margin-top", (-10+((wScroll*10)/625))+"%");
+    $("#AboutMeDiv2").css("margin-top", (-8+((wScroll*.1)/625))+"%");
+    $("#AboutMeDiv2").css("margin-right", (40-(wScroll*.01)+"%"));
     $("#ParisSelfie").css("background-position", "right " + (500+(((wScroll*.5)*.5)*-1))+"px");
     $("#SpeakIT").css("background-position", "left " + (730+(((wScroll*.5)*.5)*-1))+"px");
     $("#GatheRVA").css("background-position", "right " + (900+(((wScroll*.5)*.5)*-1))+"px");
     $("#Bamazon").css("background-position", "left " + (1050+(((wScroll*.5)*.5)*-1))+"px");
     $("#Giftastic").css("background-position", "right " + (1200+(((wScroll*.5)*.5)*-1))+"px");
+
+    console.log("this is horizontal scroll: "+(10+((wScroll*.05))))
 }
+
